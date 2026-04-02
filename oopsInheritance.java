2ace24c80d43c2141cc9df7d33ea9155eda41980
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-02 18:55:54 +0530

    Update oopsInheritance.java

--- oopsInheritance.java
+++ oopsInheritance.java
@@ -1,5 +1,5 @@
 package Jan_Batch_2026;
-
+/*
 // single inheritance
 
 class Warehouse{
@@ -30,5 +30,80 @@ public class OopsInheritance {
 		w.product();
 
 	}
-
 }
+*/
+//multilevel-inheritance
+/*
+class gp{
+	public void gpassets() {
+		System.out.println("5 crores");
+	}
+}
+class p extends gp{
+	public void passets() {
+		System.out.println("10 crores");
+	}
+}
+class c extends p{
+	public void cassets() {
+		System.out.println("nil");
+	}
+}
+public class OopsInheritance {
+	public static void main(String[] args) {
+		gp g = new gp();
+		g.gpassets();
+		
+		p pa = new p();
+		pa.gpassets();
+		pa.passets();
+		
+		c ch = new c();
+		ch.gpassets();
+		ch.passets();
+		ch.cassets();
+	}
+}
+*/
+ class resume{
+	 public void resume1() {
+		 System.out.println("Name \nObjective \nEducation \nProject");
+	 }
+ }
+ class resumeTem1 extends resume{
+	 public void resumeT1() {
+		 System.out.println("Straight Format with no color");
+	 }
+ }
+ class resumeTem2 extends resume{
+	 public void resumeT2() {
+		 System.out.println("Straight Format with color");
+	 }
+ }
+ class resumeTem3 extends resume{
+	 public void resumeT3() {
+		 System.out.println("Table Format with color");
+	 }
+ }
+public class OopsInheritance {
+	public static void main(String[] args) {
+		resumeTem1 r1 = new resumeTem1();
+		r1.resume1();
+		r1.resumeT1();
+		System.out.println();
+		
+		resumeTem2 r2 = new resumeTem2();
+		r2.resume1();
+		r2.resumeT2();
+		System.out.println();
+		
+		resumeTem3 r3 = new resumeTem3();
+		r3.resume1();
+		r3.resumeT3();
+		System.out.println();
+	}
+}
+
+
+
+
